df0aeea7b8e2603cf1c74e49a9704390ed3f36ba
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-14 10:54:11 -0800

    update index.js to include test.js and path

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Test from './modules/Test'
 import{ Router, Route, hashHistory } from 'react-router'
 
 render((
@@ -10,5 +11,6 @@ render((
     <Route path="/" component={App}/>
     <Router path="/repos" component={Repos}/>
     <Router path="/about" component={About}/>
+    <Router path="/test" component={Test}/>
   </Router>
 ), document.getElementById('app'))

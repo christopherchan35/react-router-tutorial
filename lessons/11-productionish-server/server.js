5333e37abe6252fcf12549c91819772558f4dbc8
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-14 16:37:06 -0800

    unexpected token in bundle.js

--- lessons/11-productionish-server/server.js
+++ lessons/11-productionish-server/server.js
@@ -3,8 +3,10 @@ var path = require('path')
 
 var app = express()
 
+// serve our static stuff like index.css
 app.use(express.static(__dirname))
 
+// send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
